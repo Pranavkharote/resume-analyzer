b4d139f48ac0a36cfa3e135a783564ddd9aa02fe
Pranav Satish Kharote <pranavkharote2005@gmail.com>
2025-09-19 22:32:03 +0530

    refactor: simplify App component and add Content component for file upload

---
+++ frontend/src/components/Content.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Content = () => {
+  return (
+     <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Resume Analyzer</h1>
+      <input type="file" accept=".pdf" />
+    </div>
+  )
+}
+
+export default Content
