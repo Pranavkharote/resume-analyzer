47c51c848ac20f1d0d1aa86acaf6c2684230626b
Pranav Satish Kharote <pranavkharote2005@gmail.com>
2025-11-03 13:04:18 +0530

    refactor: remove unused activity routes for cleaner user routing

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -5,7 +5,5 @@ const router = Router();
 
 router.route("/login").post(login);
 router.route("/register").post(register);
-router.route("/add_to_activity");
-router.route("/get_all_activity");
 
 module.exports =  router;
